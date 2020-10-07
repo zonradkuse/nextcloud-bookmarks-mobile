eb64652c01aca22c2f4bf7c4a0cca6463e58119a
Johannes Neuhaus <neuhaus@entenhausen.io>
2020-10-07 22:45:46 +0200

    Clean imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
-import 'package:bookmarks/models/User.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:sqflite/sqflite.dart';
-import 'package:path/path.dart';
 
 import 'layouts/DefaultLayout.dart';
 
