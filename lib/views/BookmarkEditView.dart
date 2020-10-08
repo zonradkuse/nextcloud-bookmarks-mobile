b1395fa98d1657550e1fce74a700dee06d784928
Johannes Neuhaus <neuhaus@entenhausen.io>
2020-10-08 21:31:15 +0200

    Add empty BookmarkEditView

---
+++ lib/views/BookmarkEditView.dart
@@ -0,0 +1,16 @@
+import 'package:bookmarks/abstractions/AuthenticatedView.dart';
+import 'package:bookmarks/controllers/BookmarkController.dart';
+import 'package:bookmarks/widgets/HomeWidget.dart';
+import 'package:flutter/material.dart';
+
+class BookmarkEditView
+    extends AuthenticatedView<HomeWidget, BookmarkController> {
+  const BookmarkEditView(state, {Key key}) : super(state, key: key);
+
+  Widget doBuild(BuildContext context) {
+    return Stepper(
+        steps: null
+    );
+  }
+
+}
